72b486d0d4efc4cc1693f59b598a808136405b25
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-11-30 16:47:35 -0700

    add nav and hero image components

---
+++ src/components/HeroImage.jsx
@@ -0,0 +1,16 @@
+function HeroImage(){
+   
+    return(
+
+    <div className="Hero">
+        <img className="heroimg" src="../public/iphone.png" alt="test"/>
+        <h1 className="centered">A place for creatives to build their portfolio</h1>
+        <h5 className="centered-below">Magnetize those connections!!!</h5>
+        <button className="">Get Started</button>
+    </div>
+
+)
+
+}
+
+export default HeroImage
---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+function Nav() {
+    return(
+        <nav>
+        <h3>MagNet</h3>
+        <ul className="nav-list">
+          <li>Community</li>
+          <li>Contact</li>
+          <li>About</li>
+          <li>Pricing</li>
+        </ul>
+      </nav>
+    )
+
+}
+
+export default Nav 
