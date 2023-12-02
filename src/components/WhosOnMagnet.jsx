16b394389597c0aabaeea839fc258f273bba743b
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-02 09:36:46 -0700

    start WhosOnMagNet styling

--- src/components/WhosOnMagnet.jsx
+++ src/components/WhosOnMagnet.jsx
@@ -4,13 +4,13 @@ import BusinessTypes from "./BusinessTypes"
 function WhosOnMagnet(){
 
     return(
-        <div>
-            <h2>Check out who's using MagNet:</h2>
+        <div className="bg-red-600">
+            <h2 className="py-12 text-6xl">Check out who's using MagNet:</h2>
 
-            <h3>Creatives</h3>
+            <h3 className="pb-8 text-3xl">Creatives</h3>
             <CreativeTypes />
 
-            <h3>Businesses</h3>
+            <h3 className="pb-8 text-3xl">Businesses</h3>
             <BusinessTypes/>
         
         </div>
