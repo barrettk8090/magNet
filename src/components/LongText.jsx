ebfa0d7662a03e8b68b862ec7be358a15e9ca496
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-01 18:23:43 -0700

    testimonials copy

--- src/components/LongText.jsx
+++ src/components/LongText.jsx
@@ -3,7 +3,7 @@ function LongText(){
     return(
         <div id="how-it-works">
             <h2>Build Your Portfolio. Kickstart Your Brand. </h2>
-            <h5>Magnet brings people together. We offer a space for new creatives to build their portfolios while helping small businesses get up and running. </h5>
+            <h5>Magnet brings people together. We offer a space for new creatives to build their portfolios while helping small businesses get up and running. As a budding creative, you can offer your services to individuals who are looking for help: from product photography, to video editing, to building a clients first-ever website. As a small business, you can post requests for services. It all happens on Magnet.</h5>
         </div>
     )
 }
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,8 +5,8 @@ function Testimonials(){
             <h3>Testimonials</h3>
             <div>
                 <img />
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore."</p>
-                <h5>Persons Name</h5>
+                <p>"I recently ended a career in corporate finance to pursue my passion for product photography. Once I did that, though, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
+                <h5>Joseph Edwards</h5>
             </div>
 
             <div>
