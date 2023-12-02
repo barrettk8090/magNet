5f8d7e4719d4f87765c4fbddd0050a71eefee8e7
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-02 09:16:52 -0700

    start testimonials styling

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,15 +2,15 @@ function Testimonials(){
 
     return(
         <div className="bg-red-600">
-            <h3>Testimonials</h3>
-            <div>
-                <img />
+            <h3 className="pt-12 text-6xl">Testimonials</h3>
+            <div className="px-8 py-8">
+                <img className="rounded-full h-20" src="../public/barrett-k.png" />
                 <p>"I recently ended a career in corporate finance to pursue my passion for product photography. Once I did that, though, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
                 <h5>Joseph Edwards</h5>
             </div>
 
-            <div>
-                <img />
+            <div className="px-8 py-8">
+                <img className="rounded-full h-20" src="../public/barrett-k.png" />
                 <p>"I'm new to residential real estate, and I knew that if I had a simple website to promote myself, I'd have better luck at gaining new clients. Unfortunately, I'm not very good with computers, and I definitely could not afford spending money on development services in this economy! Luckily, I found Magnet, where I was paired with a software development student who was looking to build websites to get more front-end experience. I couldn't be happier with the end result!"</p>
                 <h5>Jasmine Hartwell</h5>
             </div>
