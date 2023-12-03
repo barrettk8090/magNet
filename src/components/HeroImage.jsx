e79ac783c68837e7120b1645ff1a2f0628d45023
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-03 08:10:06 -0700

    fix nav and hero padding

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,16 +2,16 @@ function HeroImage(){
    
     return(
 
-    <div className="Hero md:flex md:flex-row items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="Hero pt-40 md:pt-16 md:flex md:flex-row items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
 
-        <div className="textdiv md:basis-1/2">
+        <div className="textdiv md:basis-2/3">
             <h1 className="text-9xl pt-14 font:display" >magNet</h1>
             <h2 className="mx-8 py-6 text-4xl">Where creatives develop their portfolios & small businesses build their brands.</h2>
             <button className="mt-12"> ↓ How It Works ↓</button>
         </div>
 
 
-        <div className="imagediv md:basis-1/2">
+        <div className="imagediv md:basis-1/3">
             <img className="heroimg" src="../public/citysky.jpeg" alt="test"/>
         </div>
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,11 +3,12 @@ import { Link, NavLink, useParams } from "react-router-dom";
 
 function Nav() {
     return(
-        <nav className="h-20 flex pl-4 fixed top-0 bg-slate-900 w-full">
+      
+        <nav className="h-20 flex pl-8 pt-1 fixed top-0 bg-slate-900 w-full">
          
           <h3 className="font-display text-6xl py-3 pl-1"><Link to="/" className="text-white">M</Link></h3>
           
-          <ul className="py-5 ml-12 flex flex-row space-x-12">
+          <ul className="py-5 ml-12 flex flex-row space-x-14">
             <li ><Link to="/community" className="text-white">Community</Link></li>
             <li><Link to="/contact" className="text-white">Contact</Link></li>
             <li><Link to="/about" className="text-white">About</Link></li>
