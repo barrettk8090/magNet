739939f469f7b88e177c52477617a3013a033066
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-04 10:39:03 -0700

    fix reply button styling on contact post cards

--- src/components/ContactPost.jsx
+++ src/components/ContactPost.jsx
@@ -1,13 +1,15 @@
 function ContactPost({post}){
 
     return(
-            <div className="bg-sky-950 rounded-lg">
+            <div className="bg-sky-950 rounded-lg grid">
                 <div className="px-6 py-6">
-                <p className="text-2xl font-extrabold"> {post.title}</p>
-                <p className="pt-8"> "{post.message}"</p>
-                <p className="pt-8">  {post.firstName} {post.lastName} </p>
-                <p>  {post.email}</p>
-                <button className="mt-4">Reply →</button>
+                    <p className="text-2xl font-extrabold"> {post.title}</p>
+                    <p className="pt-8"> "{post.message}"</p>
+                    <p className="pt-8">  {post.firstName} {post.lastName} </p>
+                    <p>  {post.email}</p>
+                </div>
+                <div className="grid grid-cols-1 place-items-end">
+                        <button className="mr-4 mb-4">Reply →</button>
                 </div>
             </div>
             )
