163e9a4de2c3ea2d8494e89658b8790fe1cf700f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-04 13:39:43 -0700

    copy and slider changes to testimonials carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,27 +5,49 @@ import 'pure-react-carousel/dist/react-carousel.es.css';
 const Carousel = () => {
   return ( 
     <CarouselProvider
-      naturalSlideWidth={100}
-      naturalSlideHeight={100}
+    naturalSlideWidth={100}
+    naturalSlideHeight={100}
       totalSlides={3}
     >
         <h2 className="text-3xl pb-12"> Testimonials</h2>
+
+       <div className="grid grid-cols-2"> 
         <ButtonBack className="border border-sky-500"> Back </ButtonBack>
-      <ButtonNext className="border border-sky-500">Next</ButtonNext>
+        <ButtonNext className="border border-sky-500">Next</ButtonNext>
+      </div>
+
       <Slider>
         
         <Slide index={0}>
           <div className="rounded-lg bg-purple-600 mx-12 my-12">
             <p className="px-4 py-4 text-xl"> "I recently left a career in corporate finance to pursue my passion for product photography. Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!!"</p>
-            <p className="px-4 py-1 text-xl">Person's Name</p>
-            <p className="px-4 py-1 text-xl">Perons's Profession</p>
+            <p className="px-4 py-1 text-xl">Jospeph Edwards</p>
+            <p className="px-4 py-1 text-xl">Photographer</p>
           </div>
 
             
             
         </Slide>
-        <Slide index={1}>Slide 2</Slide>
-        <Slide index={2}>Slide 3</Slide>
+        <Slide index={1}>
+
+        <div className="rounded-lg bg-purple-600 mx-12 my-12">
+            <p className="px-4 py-4 text-xl"> "I'm new to residential real estate, and I knew that if I had a simple website to promote myself, I'd have better luck at gaining new clients. Unfortunately, I'm not very good with computers, and I definitely could not afford spending money on development services in this economy! Luckily, I found Magnet, where I was paired with a software development student who was looking to build websites to get more front-end experience. I couldn't be happier with the end result!"</p>
+            <p className="px-4 py-1 text-xl">Jasmine Hartwell</p>
+            <p className="px-4 py-1 text-xl">Realtor</p>
+          </div>
+        
+        
+        </Slide>
+        <Slide index={2}>
+
+        <div className="rounded-lg bg-purple-600 mx-12 my-12">
+            <p className="px-4 py-4 text-xl"> "I just completed a graphic design bootcamp, which was awesome! The hard part was finding my first real client for my portfolio. Luckily, I found Magnet, where I was matched with the owner of a new food truck in town. They were looking for some help with designing their menu, and I gladly accepted. Now my work is featured on the side of their food truck, and they even put a QR code that directs to my website on there! I've already gotten 6 paying clients from the exposure!"</p>
+            <p className="px-4 py-1 text-xl">Sam Johnson</p>
+            <p className="px-4 py-1 text-xl">Graphic Designer</p>
+          </div>
+
+
+        </Slide>
       </Slider>
 
     </CarouselProvider>
