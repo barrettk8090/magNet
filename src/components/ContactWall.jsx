4178530999573696e765420b5e82641d9eb4e0fd
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-04 10:08:39 -0700

    center contact us title

--- src/components/ContactWall.jsx
+++ src/components/ContactWall.jsx
@@ -10,7 +10,7 @@ function ContactWall({posts}){
     
     return(
 
-        <div>
+        <div className="mt-10 mx-6 md:mx-16 lg:mx-40 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-1">
             {displayPosts}
         </div>
     )
