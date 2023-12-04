2bc21f3c1b49a854c1cad6995743bb49d6c56a1f
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-04 15:09:37 -0700

    add phone images and adjust padding in hero

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,17 +2,18 @@ function HeroImage(){
    
     return(
 
-    <div className="Hero pt-40 md:pt-16 md:flex md:flex-row items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="pt-40 lg:pt-24 md:flex md:flex-row items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
 
-        <div className="textdiv md:basis-2/3">
+        <div className="textdiv md:basis-1/2">
             <h1 className="text-9xl pt-14 font:display" >magNet</h1>
             <h2 className="mx-8 py-6 text-4xl font:body font-medium">Where creatives develop their portfolios & small businesses build their brands.</h2>
-            <button className="mt-12"> ↓ How It Works ↓</button>
+            <button className="mt-12 mx-6 hover:text-black hover:bg-white"> Download for iOS </button>
+            <button className="mt-12 hover:text-black hover:bg-white"> Download for Android Ⓖ</button>
         </div>
 
 
-        <div className="imagediv md:basis-1/3">
-            <img className="heroimg" src="../public/citysky.jpeg" alt="test"/>
+        <div className="md:basis-1/2 pt-20 mx-12">
+            <img className="" src="../public/phones.png" alt="test"/>
         </div>
 
       
