ae2e4a128aff6f9d8c0a21bbc1c2c82f4ea92156
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-05 10:41:02 -0700

    long text copy and h5 updates

--- src/components/HeroImage.jsx
+++ src/components/HeroImage.jsx
@@ -2,13 +2,15 @@ function HeroImage(){
    
     return(
 
-    <div className="pt-40 lg:pt-24 md:flex md:flex-row items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+    <div className="pt-40 lg:pt-16 md:pb-20 md:flex md:flex-row items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
 
         <div className="textdiv md:basis-1/2">
             <h1 className="text-9xl pt-14 lg:pt-0 font:display select-none" >magNet</h1>
-            <h2 className="mx-8 py-6 text-4xl font:body font-medium select-none">Where creatives develop their portfolios & small businesses build their brands.</h2>
-            <button className="mt-12 mx-6 hover:text-black hover:bg-white"> Download for iOS </button>
-            <button className="mt-12 hover:text-black hover:bg-white"> Download for Android Ⓖ</button>
+            <h2 className="mx-8 md:mx-4 lg:mx-8 py-6 text-4xl font:body font-medium select-none">Where creatives develop their portfolios & small businesses build their brands.</h2>
+            <div className="mt-12">
+                <button className="mx-4 md:mb-4 hover:text-black hover:bg-white"> Download for iOS </button>
+                <button className="mx-4 md:mt-4 hover:text-black hover:bg-white"> Download for Android Ⓖ</button>
+            </div>
         </div>
 
 
--- src/components/LongText.jsx
+++ src/components/LongText.jsx
@@ -4,10 +4,15 @@ function LongText(){
 
     return(
         <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
-            <div className="xl:mx-60 lg:py-60 lg:mx-40">
+            <div className="xl:mx-60 lg:pt-32 lg:pb-60 lg:mx-40">
                 <h2 className="pt-12 px-8 text-6xl font-medium">Build Your Portfolio.  </h2>
                 <h2 className="pb-12 lg:pb-24 px-8 text-6xl font-medium">Kickstart Your Brand.</h2>
-                <h5 className="pb-12 px-12 text-3xl leading-relaxed">Magnet brings people together. We offer a space for new creatives to build their portfolios while helping small businesses get up and running. As a budding creative, you can offer your services to individuals who are looking for help: from product photography, to video editing, to building a clients first-ever website. As a small business, you can post requests for services. It all happens on Magnet.</h5>
+                <h5 className="pb-12 px-12 text-3xl leading-relaxed">Hi 👋 We’re Magnet. </h5>
+                <h5 className="pb-12 px-12 text-3xl leading-relaxed">
+                We’re designed to help new creatives in building their portfolios, and to get small businesses up and running quickly. Magnet is powered by the desire we have to help our fellow human beings succeed. Whether you’re a new creative looking to expand your portfolio, or a new independent business that could use a little help getting started, there’s a connection waiting for you on Magnet.</h5>
+                <h5 className="pb-12 px-12 text-3xl leading-relaxed" >
+                Magnet is specifically designed for those who are just beginning their first venture and need a lifting off point.
+                </h5>
             </div>
         </div>
     )
