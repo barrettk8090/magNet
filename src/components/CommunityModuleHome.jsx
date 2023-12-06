59d8e0d1c4da15ab6d2e1f010b94dcda1444d8e3
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-06 08:59:46 -0700

    fix community See More Connections button spacing

--- src/components/CommunityModuleHome.jsx
+++ src/components/CommunityModuleHome.jsx
@@ -4,12 +4,12 @@ function CommunityModuleHome(){
 
 
     return (
-        <div className="bg-sky-600 py-20 px-12 lg:mx-2 xl:mx-8">
+        <div className="bg-sky-600 pt-20 px-12 lg:mx-2 xl:mx-8">
             <h3 className="pb-12 text-9xl font-display font-medium">Community</h3>
 
             <CommunityCardHome/>
             <div className="flex justify-center">
-                <button className="mb-60">See More Connections →</button>
+                <button className="mt-10 mb-20">See More Connections →</button>
             </div>
 
         </div>
