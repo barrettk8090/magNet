4b92c52b35ed9e1901af7598ef9ed21538daa172
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-06 09:43:06 -0700

    fully responsive testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,28 +3,30 @@ function Testimonials(){
     return(
         <div className="pt-20 px-12 lg:mx-2 xl:mx-8">
             <h3 className="pb-12 text-9xl font-display font-medium">Testimonials</h3>
+        <div className="lg:grid lg:grid-cols-2">
+            <div className="lg:col-span-1 lg:mx-4 xl:mx-8">
+                <div className="mx-2 border-white rounded-lg shadow-xl">
 
-            <div className="mx-2 border-white rounded-lg shadow-xl">
+                    <div className="px-8 py-8 rounded-lg bg-white">
+                        
+                        <p className="pb-4 text-black font-medium text-2xl">"I recently ended a career in corporate finance to pursue my passion for product photography. </p>
+                        <p className="text-black font-medium"> Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
 
-                <div className="px-8 py-8 rounded-lg bg-white">
+                    </div>
                     
-                    <p className="pb-4 text-black font-medium text-2xl">"I recently ended a career in corporate finance to pursue my passion for product photography. </p>
-                    <p className="text-black font-medium"> Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
-
                 </div>
-                
-            </div>
 
-            <div className="flex">
-                <img className="rounded-full h-20 mx-4 my-8" src="../public/barrett-k.png" />
-                <div className="flex flex-col">
-                    <h5 className="pt-12 font-bold">Joseph Edwards</h5>
-                    <h5 className="py-0">Photographer</h5>
+                <div className="flex">
+                    <img className="rounded-full h-20 mx-4 my-8" src="../public/barrett-k.png" />
+                    <div className="flex flex-col">
+                        <h5 className="pt-12 font-bold">Joseph Edwards</h5>
+                        <h5 className="py-0">Photographer</h5>
+                    </div>
                 </div>
             </div>
 
             <div className="mx-2 border-white rounded-lg shadow-xl">
-
+            <div className="lg:col-span-1 lg:mx-4 xl:mx-8">
                 <div className="px-8 py-8 rounded-lg bg-white">
                     
                     <p className="pb-4 text-black font-medium text-2xl">"I'm new to residential real estate, and I knew that if I had a simple website to promote myself, I'd have better luck at gaining new clients.</p>
@@ -41,6 +43,8 @@ function Testimonials(){
                     <h5 className="py-0">Realtor</h5>
                 </div>
             </div>
+            </div>
+        </div>
         </div>
     )
 }
