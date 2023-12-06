5a640fc2ec3c2f806d47e5be779615c308bf0194
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-06 09:44:09 -0700

    font weight adjustment on testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -10,7 +10,7 @@ function Testimonials(){
                     <div className="px-8 py-8 rounded-lg bg-white">
                         
                         <p className="pb-4 text-black font-medium text-2xl">"I recently ended a career in corporate finance to pursue my passion for product photography. </p>
-                        <p className="text-black font-medium"> Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
+                        <p className="text-black font-light"> Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
 
                     </div>
                     
@@ -30,7 +30,7 @@ function Testimonials(){
                 <div className="px-8 py-8 rounded-lg bg-white">
                     
                     <p className="pb-4 text-black font-medium text-2xl">"I'm new to residential real estate, and I knew that if I had a simple website to promote myself, I'd have better luck at gaining new clients.</p>
-                    <p className="text-black font-medium"> Unfortunately, I'm not very good with computers, and I definitely could not afford spending money on development services in this economy! Luckily, I found Magnet, where I was paired with a software development student who was looking to build websites to get more front-end experience. I couldn't be happier with the end result!"</p>
+                    <p className="text-black font-light"> Unfortunately, I'm not very good with computers, and I definitely could not afford spending money on development services in this economy! Luckily, I found Magnet, where I was paired with a software development student who was looking to build websites to get more front-end experience. I couldn't be happier with the end result!"</p>
 
                 </div>
                 
