a52ec7814f1d819be5be929f182fded57e893623
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-06 14:36:53 -0700

    testimonials styling and fix static nav

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,16 +1,16 @@
 function Testimonials(){
 
     return(
-        <div className="pt-20 px-12 lg:mx-2 xl:mx-8">
-            <h3 className="pb-12 text-9xl font-display font-medium">Testimonials</h3>
-        <div className="lg:grid lg:grid-cols-2">
-            <div className="lg:col-span-1 lg:mx-4 xl:mx-32">
-                <div className="mx-2 border-white rounded-lg shadow-xl">
+        <div className="pt-20 px-12 lg:mx-2 xl:mx-8 my-32">
+            <h3 className="xl:mx-32 pb-12 text-9xl font-display font-medium">Testimonials</h3>
+        <div className="lg:grid lg:grid-cols-2 xl:mx-32">
+            <div className="lg:col-span-1 lg:mx-4 ">
+                <div className="mx-2 lg:mx-20 border-white rounded-lg shadow-xl">
 
                     <div className="px-8 py-8 rounded-lg bg-white">
                         
-                        <p className="pb-4 text-black font-medium text-2xl">"I recently ended a career in corporate finance to pursue my passion for product photography. </p>
-                        <p className="text-black font-light"> Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
+                        <p className="pb-4 text-black font-medium text-2xl lg:text-3xl xl:text-4xl">"I recently ended a career in corporate finance to pursue my passion for product photography. </p>
+                        <p className="text-black font-light xl:text-xl"> Inititally, I felt lost – I had no idea where to begin. After cold emailing hundreds of businesses with no response, I realized my portfolio needed a facelift. Then, I found out about Magnet, where I was paired with a small beauty brand that was struggling to get their product photos to look perfect. I was able to take photos for them, add those images to my portfolio, and since then, I just got my first paying client! Thanks Magnet!! "</p>
 
                     </div>
                     
@@ -25,12 +25,12 @@ function Testimonials(){
                 </div>
             </div>
 
-            <div className="mx-2 border-white rounded-lg shadow-xl">
-            <div className="lg:col-span-1 lg:mx-4 xl:mx-32">
-                <div className="px-8 py-8 rounded-lg bg-white">
+            <div className="mx-2 lg:mx-20 border-white rounded-lg">
+            <div className="lg:col-span-1 lg:mx-4 ">
+                <div className="px-8 py-8 rounded-lg bg-white  shadow-xl">
                     
-                    <p className="pb-4 text-black font-medium text-2xl">"I'm new to residential real estate, and I knew that if I had a simple website to promote myself, I'd have better luck at gaining new clients.</p>
-                    <p className="text-black font-light"> Unfortunately, I'm not very good with computers, and I definitely could not afford spending money on development services in this economy! Luckily, I found Magnet, where I was paired with a software development student who was looking to build websites to get more front-end experience. I couldn't be happier with the end result!"</p>
+                    <p className="pb-4 text-black font-medium text-2xl lg:text-3xl xl:text-4xl">"I'm new to residential real estate, and I knew that if I had a simple website to promote myself, I'd have better luck at gaining new clients.</p>
+                    <p className="text-black font-light xl:text-xl"> Unfortunately, I'm not very good with computers, and I definitely could not afford spending money on development services in this economy! Luckily, I found Magnet, where I was paired with a software development student who was looking to build websites to get more front-end experience. I couldn't be happier with the end result!"</p>
 
                 </div>
                 
