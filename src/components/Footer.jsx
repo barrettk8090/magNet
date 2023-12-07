7dab670bf317c1844fab5b296bb7c748a14eb6d5
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-07 13:56:18 -0700

    minor footer styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ function Footer(){
     return(
         <footer className="pb-20 bg-black grid justify-items-center md:grid-cols-2">
 
-            <div className="py-20">
+            <div className="pl-2 py-10 ">
                 <h5 className="font-display text-6xl">Sign up for our newsletter!</h5>
                 <p className="pt-4 pb-6">Tips, tricks, and updates sent straight to your inbox.</p>
-                <input className="rounded h-10" placeholder="Enter your email"/>
+                <input className="rounded h-10 ml-6 sm:ml-0 pl-2" placeholder="Enter your email"/>
                 <button>Sign Up →</button>
             </div>
 
