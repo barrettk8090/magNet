59d3d3b45cf0e9878ef4872a07d68b5c0ec18e70
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-08 11:50:10 -0700

    final creative biz types color updates

--- src/components/BusinessTypes.jsx
+++ src/components/BusinessTypes.jsx
@@ -20,7 +20,7 @@ function BusinessTypes(){
         <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Builders 🏗️ </span>
         <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Lawyers ⚖️ </span>
         <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Professors 🤓 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-600 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600"><Link className="text-white" to="/community" >And Many More →</Link></span> 
+        <span className="rounded-lg border-solid border-2 border-sky-950 bg-indigo-500 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-pink-500"><Link className="text-white" to="/community" >And Many More →</Link></span> 
         </div>
     )
 }
