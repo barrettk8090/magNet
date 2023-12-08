6760e68719a7bd0f1487942099d290f34bfef844
Barrett Kowalsky <barrettkowalsky@gmail.com>
2023-12-08 11:46:12 -0700

    update creative biz types colors

--- src/components/BusinessTypes.jsx
+++ src/components/BusinessTypes.jsx
@@ -4,22 +4,22 @@ function BusinessTypes(){
 
     return (
         <div className="flex flex-wrap">
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg hover:bg-sky-600">Realtors 🏡</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Restaurants 🍝</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Cafés ☕️</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Jewelry Makers 💍 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Food Trucks 🚚 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Soap Makers 🧼</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Small Biz 🛒</span> 
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Morgues ⚰️ </span> 
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Hairstylists 💇‍♀️ </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Chefs 🍴 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Bloggers 💻 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Bookstores 📚 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Construction 🚧 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Builders 🏗️ </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Lawyers ⚖️ </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Professors 🤓 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Realtors 🏡</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Restaurants 🍝</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Cafés ☕️</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Jewelry Makers 💍 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Food Trucks 🚚 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Soap Makers 🧼</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Small Biz 🛒</span> 
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Morgues ⚰️ </span> 
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Hairstylists 💇‍♀️ </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Chefs 🍴 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Bloggers 💻 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Bookstores 📚 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Construction 🚧 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Builders 🏗️ </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Lawyers ⚖️ </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Professors 🤓 </span>
         <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-600 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600"><Link className="text-white" to="/community" >And Many More →</Link></span> 
         </div>
     )
--- src/components/CreativeTypes.jsx
+++ src/components/CreativeTypes.jsx
@@ -4,21 +4,21 @@ function CreativeTypes(){
 
     return (
        <div className="flex flex-wrap">
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg hover:bg-sky-600">Photographers 📸</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Videographers 📹</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Video Editors 🎞️</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Illustrators 🖌️ </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">UX Designers ⚙️</span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">UI Designers 📱</span> 
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Programmers 💻 </span> 
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Drone Operators 🚁 </span> 
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Knitters 🧶 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Hairstylists 💇‍♀️ </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Makeup Artists 💄 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Jewelry Makers 💍 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">3D Modelers🗿 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Writers 📝 </span>
-        <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-950 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600">Artists 🎨  </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Photographers 📸</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Videographers 📹</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Video Editors 🎞️</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Illustrators 🖌️ </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">UX Designers ⚙️</span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">UI Designers 📱</span> 
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Programmers 💻 </span> 
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Drone Operators 🚁 </span> 
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Knitters 🧶 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Hairstylists 💇‍♀️ </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Makeup Artists 💄 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Jewelry Makers 💍 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">3D Modelers🗿 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Writers 📝 </span>
+        <span className="rounded-lg border-solid border-2 bg-white px-4 py-2 ml-1 mr-1 my-2 drop-shadow-lg text-black hover:bg-pink-500 hover:text-white hover:border-pink-500">Artists 🎨  </span>
     
         <span className="rounded-lg border-solid border-2 border-sky-950 bg-sky-600 px-4 py-2 mx-1 my-2 drop-shadow-lg hover:bg-sky-600"><Link className="text-white" to="/community" >And Many More →</Link></span> 
         
